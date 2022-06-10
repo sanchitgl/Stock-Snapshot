db9c30306779c79c98489a5a0beed0d74b48f7b5
sanchitgoel7 <105559691+sanchitgoel7@users.noreply.github.com>
2022-06-10 18:12:12 +0530

    changed dcf Year 0 cashflow

--- pages/03_❤️_Support.py
+++ pages/03_❤️_Support.py
@@ -27,7 +27,7 @@ with col2:
 col1, col2, col3 = st.columns([1,1.5,1])
 with col2: 
     components.html(''''
-        <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="stocksnapshot" data-color="#FFDD00" data-emoji="🍵"  data-font="Cookie" data-text="Buy me a green tea" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
+        <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="stocksnapshot" data-color="#FFDD00" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee?" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
     '''
         )
     # components.html('''
